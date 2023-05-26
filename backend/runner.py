b13ee9aa8d88818d1a3d8318935e06b994379d8b
Ethan Haffenden <z5367576@ad.unsw.edu.au>
2023-05-26 22:19:12 +1000

    Implemented functionality so it asks for a confirmation for the email before it runs

--- backend/runner.py
+++ backend/runner.py
@@ -3,6 +3,7 @@ import smtplib
 import edit_content.sensative_info as sensative_info
 from backend.email_builder import emailBuilder
 from backend.csv_extractor import csvToDictList
+from backend.email_confirmation import emailConfirmation
 from backend.errors import checkFilledLogin, checkEmailFormatted
 
 def runner() -> int:
@@ -17,6 +18,8 @@ def runner() -> int:
     s.login(sensative_info.LOGIN_EMAIL, sensative_info.LOGIN_PASSWORD)
     
     rcpnts = csvToDictList()
+
+    emailConfirmation(rcpnts)
 
     for rcpnt in rcpnts:
         email = emailBuilder(rcpnt)
