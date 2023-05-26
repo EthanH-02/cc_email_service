b8325a22ec8a2adac8fb2a3eb3b7dd4c8ece7a7b
Ethan Haffenden <z5367576@ad.unsw.edu.au>
2023-05-26 11:22:07 +1000

    Refactored code to ensure it can run considering the new formatting of the code, linking the seperate services together

--- backend/email_builder.py
+++ backend/email_builder.py
@@ -1,17 +1,20 @@
 from email.message import EmailMessage
+from message_formater import formatMessage
+import edit_content.sensative_info as sensative_info
 
-def emailBuilder(sender:str, recipient:str, subj:str) -> EmailMessage:
+def emailBuilder(rcpnt:dict) -> EmailMessage:
     
     msg = EmailMessage()
 
+    msg["From"] = sensative_info.LOGIN_EMAIL
+    msg["To"] = rcpnt['email']
+
     # Extract the information regarding the email
     with open('email_info.txt', 'r') as info_file:
-        msg["From"] = info_file.readline().split()[1]
-        msg["To"] = info_file.readline().split()[1]
         msg["Subject"] = " ".join(info_file.readline().split()[1:])
 
     # Extract the infromation regarding the contents of the email
     with open('email_contents.txt', 'r') as content_file:
-        msg.set_content(content_file.read())
+        msg.set_content(formatMessage(content_file.read(), rcpnt))
 
     return msg
--- backend/runner.py
+++ backend/runner.py
@@ -2,18 +2,20 @@ import smtplib
 
 import edit_content.sensative_info as sensative_info
 from email_builder import emailBuilder
+from csv_extractor import csvToDictList
 
-def email_sender() -> int:
+def runner() -> int:
     s = smtplib.SMTP('smtp.gmail.com', 587)
     s.starttls()
 
     s.login(sensative_info.LOGIN_EMAIL, sensative_info.LOGIN_PASSWORD)
     
-    # Create the message to be sent
-    msg = emailBuilder('', '', '')
-    
-    # SEND THE MESSAGE
-    s.send_message(msg)
+    rcpnts = csvToDictList
+
+    for rcpnt in rcpnts:
+        email = emailBuilder(rcpnt)
+        s.send_message(email)
+
     s.quit()
     
     return 0
