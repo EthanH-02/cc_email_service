b96c3594ccba78b22e084283175438d45bc93f2b
Ethan Haffenden <z5367576@ad.unsw.edu.au>
2023-05-28 21:05:22 +1000

    Implemented code in order to send multiple files through to the user as desired

--- backend/email_builder.py
+++ backend/email_builder.py
@@ -1,5 +1,6 @@
 from email.message import EmailMessage
 from backend.message_formater import formatMessage
+from backend.files_builder import getFilesInfo
 import edit_content.sensative_info as sensative_info
 
 def emailBuilder(rcpnt:dict) -> EmailMessage:
@@ -12,13 +13,14 @@ def emailBuilder(rcpnt:dict) -> EmailMessage:
     # Extract the information regarding the email
     with open('./edit_content/email_info.txt', 'r') as info_file:
         msg["Subject"] = " ".join(info_file.readline().split()[1:])
-
+ 
     # Extract the infromation regarding the contents of the email
     with open('./edit_content/email_contents.txt', 'r') as content_file:
         msg.set_content(formatMessage(content_file.read(), rcpnt))
 
-    with open('./edit_content/file.jpg', 'rb') as file:
-        content = file.read()
-        msg.add_attachment(content, maintype='application', subtype='jpg', filename='file.jpg')
+    for file_info in getFilesInfo():
+        with open(file_info.filepath, 'rb') as file:
+            content = file.read()
+            msg.add_attachment(content, maintype='application', subtype=file_info.subtype, filename=file_info.filename)
 
     return msg
